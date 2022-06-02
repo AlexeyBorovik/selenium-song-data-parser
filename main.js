c6949a8d8bec5c6baf3c2275ac8741cabb3dc1e8
RobXela <78446731+RobXela@users.noreply.github.com>
2022-06-02 15:21:35 +0300

    Fixed time array & Added LoadChecker & Save data

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const fs = require("fs");
 (async function parse() {
   let driver = new Builder().forBrowser(Browser.FIREFOX).build();
   driver.get("https://centerfm.by/ru/history/");
-  driver.manage().window().maximize();
+  let trackList = [];
+  
   try {
-    let trackList = [];
     let timeArray = [];
 
     for (let h = 0; h < 24; h++) {
       for (let m = 0; m <= 59; m += 7) {
         mh = h <= 9 ? `0${h}` : h;
-        mMin = m === 0 ? "00" : m;
+        mMin = m <= 9 ? `0${m}` : m;
         timeArray.push(`${mh}:${mMin}`);
       }
     }
@@ -24,23 +24,43 @@ const fs = require("fs");
     await dateInput.clear();
     await dateInput.sendKeys("27.05.2022");
 
+
+    let getFistTime = async () => {
+        let items = await driver.findElements(
+            By.css(".b-track-item__content-inner .b-track-item__time")
+            );
+       return items[0].getText();
+      }
+    let oldFirstTime = await getFistTime();
+
     for (time of timeArray) {
+        console.log('timeCycleChecker');
       let timeInput = await driver.findElement(
         By.name("play_history_search[time]")
       );
       let searchButton = await driver.findElement(
         By.css(".b-form-search .b-btn")
       );
+
       await timeInput.clear();
       await timeInput.sendKeys(`${time}`);
       await searchButton.click();
+      console.log(`search Clicked, ${time}`);
 
-      await PromiseDelay(15000);
-
+      while (true) {
+          console.log("LoadChecker"); 
+          await PromiseDelay(1000);
+          let newFirstTime = await getFistTime();
+          if (oldFirstTime !== newFirstTime) {
+              oldFirstTime = newFirstTime
+              break;
+          };
+       };
+      
       let contentDivs = await driver.findElements(
-        By.css(".b-track-item__content-inner")
-      );
-
+          By.css(".b-track-item__content-inner")
+          );
+                   
       for (const contentDiv of contentDivs) {
         let author = await contentDiv
           .findElement(By.css(".b-track-item__author"))
@@ -54,16 +74,24 @@ const fs = require("fs");
         trackList.push({ author, song, time });
       }
     }
-    console.log(trackList);
-    fs.writeFile("songs.json", JSON.stringify(trackList), function (err) {
+
+     fs.writeFile("songs3.json", JSON.stringify(trackList), function (err) {
         if (err) {
             console.log("An error occured while writing JSON Object to File.");
             return console.log(err);
         }
         console.log("JSON file has been saved.");
     })
+
     setTimeout(() => driver.quit(), 10000);
   } catch (error) {
+    fs.writeFile("PartOfsongs3.json", JSON.stringify(trackList), function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    })
     console.log(error);
     setTimeout(() => driver.quit(), 10000);
   }
@@ -71,4 +99,4 @@ const fs = require("fs");
 
 function PromiseDelay(delayInMs) {
   return new Promise((resolve) => setTimeout(resolve, delayInMs));
-}
+};
